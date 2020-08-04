334f7d35043c11d5c80a1ec4bf3b925c548bb34b
Jazzel Mehmood <jazzelmehmood6@gmail.com>
2020-08-04 16:02:47 +0500

    Set dev to deploy

--- project/settings.py
+++ project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'uik2s(huj(g=)%wf$ofziuor7&@=@^9*e&@e*$6s8fem)dbh47'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
